53f25b430bb0c49c0447cf6aa9f3e5b4e5daadb3
seolleung2 <seolleung22@gmail.com>
2025-01-29 23:52:53 +0900

    add deploy.yml, edit config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Mook's Devlog",
+  tagline: "Mook's Devlog",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
